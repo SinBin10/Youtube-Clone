467a1cfda4791136980086ecb9db64fa8729abde
SinBin10 <bincode10@gmail.com>
2024-09-08 18:38:54 +0530

    header component completed.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import hamburger from "../assets/Menu.png";
 import logo from "../assets/youtube-logo-png-46020.png";
 import search from "../assets/search.png";
+import mic from "../assets/Mic.png";
+import setting from "../assets/image.png";
+import user from "../assets/user.png";
 import { useState } from "react";
 
 const Header = () => {
@@ -11,16 +14,16 @@ const Header = () => {
       <div className="flex h-14">
         <div className="h-full">
           <img
-            className="h-full p-4 hover:cursor-pointer"
+            className="h-full p-4 cursor-pointer"
             src={hamburger}
             alt="hamburger"
           />
         </div>
-        <a href="/">
+        <a className="ml-2" href="/">
           <img className="h-full" src={logo} alt="youtube logo" />
         </a>
       </div>
-      <div className="h-10">
+      <div className="h-10 flex items-center">
         <div
           className={`${
             searchFocus
@@ -48,8 +51,19 @@ const Header = () => {
             />
           </div>
         </div>
+        <div className="ml-5 bg-[#ffffff26] p-2 rounded-full">
+          <img src={mic} alt="mic" />
+        </div>
       </div>
-      <div></div>
+      <div className="flex items-center pr-4">
+        <div className="h-7 mr-4 cursor-pointer">
+          <img className="h-full w-full" src={setting} alt="settings icon" />
+        </div>
+        <div className="flex border-2 py-2 px-4 rounded-3xl border-[#ffffff26] cursor-pointer">
+          <img src={user} alt="user-icon" />
+          <p className="text-blue-400 ml-2">Sign in</p>
+        </div>
+      </div>
     </div>
   );
 };
