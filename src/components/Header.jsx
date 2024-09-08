0d1fa343d8a3c09d9ecd286494657db1f8931101
SinBin10 <bincode10@gmail.com>
2024-09-08 17:02:57 +0530

    making the effect which happens when input box is clicked.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import hamburger from "../assets/Menu.png";
 import logo from "../assets/youtube-logo-png-46020.png";
 import search from "../assets/search.png";
+import { useState } from "react";
 
 const Header = () => {
+  const [searchFocus, setSearchFocus] = useState(false);
+  console.log(searchFocus);
   return (
     <div className="flex items-center">
       <div className="flex h-14">
@@ -19,13 +22,19 @@ const Header = () => {
       </div>
       <div className="h-10">
         <div className="w-[40rem] border-[#ffffff26] border-2 h-full flex rounded-3xl items-center">
-          <div className="w-12 px-3">
-            <img src={search} alt="search" />
-          </div>
+          {searchFocus && (
+            <div className="w-12 px-3">
+              <img src={search} alt="search" />
+            </div>
+          )}
           <input
-            className="h-full w-full bg-inherit text-white outline-none focus:outline-blue-500 focus:rounded-l-3xl focus:outline-1 pl-2 pr-5 text-base"
+            className={`h-full w-full ${
+              searchFocus === false ? "pl-[3.25rem]" : ""
+            } bg-inherit text-white outline-none pl-2 pr-5 text-base`}
             type="text"
             placeholder="Search"
+            onFocus={() => setSearchFocus(true)}
+            onBlur={() => setSearchFocus(false)}
           />
           <div className="h-full">
             <img
