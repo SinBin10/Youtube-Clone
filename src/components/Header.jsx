bce6915066afb613fb60fd6b1043992773cf5951
SinBin10 <bincode10@gmail.com>
2024-09-08 11:26:06 +0530

    making the search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import hamburger from "../assets/Menu.png";
 import logo from "../assets/youtube-logo-png-46020.png";
+import search from "../assets/search.png";
 
 const Header = () => {
   return (
-    <div className="flex">
+    <div className="flex items-center">
       <div className="flex h-14">
         <div className="h-full">
           <img
@@ -16,7 +17,20 @@ const Header = () => {
           <img className="h-full" src={logo} alt="youtube logo" />
         </a>
       </div>
-      <div></div>
+      <div className="h-10">
+        <div className="w-[39.5rem] border-[#ffffff26] border-2 h-full flex rounded-3xl">
+          <input
+            className="h-full w-full bg-inherit text-white outline-none focus:outline-blue-500 focus:rounded-l-3xl focus:outline-1 px-5 text-sm"
+            type="text"
+            placeholder="Search"
+          />
+          <img
+            className="py-2 px-5 rounded-r-3xl bg-[#ffffff26]"
+            src={search}
+            alt="search"
+          />
+        </div>
+      </div>
       <div></div>
     </div>
   );
