d14adcab86fa8a98c35c2ed100f45d6cd1655123
SinBin10 <bincode10@gmail.com>
2024-09-17 19:23:15 +0530

    adding more from youtube section.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,29 +4,29 @@ import user from "../assets/user.png";
 
 const Sidebar = () => {
   return (
-    <div className="w-60 p-3 hover:overflow-y-auto hover:w-[15.5rem]">
+    <div className="w-60 p-3 hover:overflow-y-auto hover:w-[15.9rem]">
       <div className="w-full pb-3 border-b-2 border-[#ffffff26]">
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Home</p>
+          <p className="pl-7">Home</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={shorts} alt="home logo" />
-          <p className="pl-10 font-semibold">Shorts</p>
+          <p className="pl-7">Shorts</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Subscriptions</p>
+          <p className="pl-7">Subscriptions</p>
         </div>
       </div>
       <div className="w-full py-3 border-b-2 border-[#ffffff26]">
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">You</p>
+          <p className="pl-7">You</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={shorts} alt="home logo" />
-          <p className="pl-10 font-semibold">History</p>
+          <p className="pl-7">History</p>
         </div>
       </div>
       <div className="w-full py-3 border-b-2 border-[#ffffff26]">
@@ -40,50 +40,64 @@ const Sidebar = () => {
         <h1 className="p-3">Explore</h1>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Trending</p>
+          <p className="pl-7">Trending</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={shorts} alt="home logo" />
-          <p className="pl-10 font-semibold">Shopping</p>
+          <p className="pl-7">Shopping</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Music</p>
+          <p className="pl-7">Music</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Movies</p>
+          <p className="pl-7">Movies</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={shorts} alt="home logo" />
-          <p className="pl-10 font-semibold">Live</p>
+          <p className="pl-7">Live</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Gaming</p>
+          <p className="pl-7">Gaming</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">News</p>
+          <p className="pl-7">News</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={shorts} alt="home logo" />
-          <p className="pl-10 font-semibold">Sports</p>
+          <p className="pl-7">Sports</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Courses</p>
+          <p className="pl-7">Courses</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={Hut} alt="home logo" />
-          <p className="pl-10 font-semibold">Fashion & Beauty</p>
+          <p className="pl-7">Fashion & Beauty</p>
         </div>
         <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
           <img className="h-5 pl-2" src={shorts} alt="home logo" />
-          <p className="pl-10 font-semibold">Podcasts</p>
+          <p className="pl-7">Podcasts</p>
         </div>
       </div>
-      <div></div>
+      <div className="w-full py-3 border-b-2 border-[#ffffff26]">
+        <h1 className="p-3">More From Youtube</h1>
+        <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
+          <img className="h-5 pl-2" src={shorts} alt="home logo" />
+          <p className="pl-7">YouTube Premium</p>
+        </div>
+        <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
+          <img className="h-5 pl-2" src={shorts} alt="home logo" />
+          <p className="pl-7">YouTube Music</p>
+        </div>
+        <div className="flex items-center p-2 rounded-xl hover:bg-[#282828] hover:cursor-pointer">
+          <img className="h-5 pl-2" src={shorts} alt="home logo" />
+          <p className="pl-7">YouTube Kids</p>
+        </div>
+      </div>
       <div></div>
       <div></div>
     </div>
